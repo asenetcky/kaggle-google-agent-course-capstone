daef1b89bba6561fb99079467e1412338f4b8f88
asenetcky <me@asenetcky.dev>
2025-11-20 14:10:46 -0500

    feat: add env vars to main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from kaggle-google-agent-course-capstone!")
+import asyncio
+import os
 
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env
+load_dotenv()
+
+try:
+    GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+    ODP_APP_TOKEN = os.environ["ODP_APP_TOKEN"]
+except Exception as e:
+    print(f"Auth error: Details: {e}") 
+
+async def main() -> None:
+    print("Hello!")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
