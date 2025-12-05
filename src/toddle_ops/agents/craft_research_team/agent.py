e5a792cc70d1ab0fa44049323caf1af74556c887
asenetcky <me@asenetcky.dev>
2025-12-04 22:38:09 -0500

    feat: add local hosted agent- reformat prompts

--- src/toddle_ops/agents/craft_research_team/agent.py
+++ src/toddle_ops/agents/craft_research_team/agent.py
@@ -1,6 +1,7 @@
 from google.adk.agents import LlmAgent, ParallelAgent, SequentialAgent
 from google.adk.models.google_llm import Gemini
 from google.adk.tools import google_search
+from google.adk.models.lite_llm import LiteLlm
 
 from toddle_ops.config import retry_config
 from toddle_ops.models.projects import Project
@@ -9,10 +10,9 @@ art_craft_researcher = LlmAgent(
     name="ArtCraftResearcher",
     model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
     instruction="""
-    Research popular and safe art crafts and projects
+    Using google_search, research safe art crafts and projects
     for toddlers that are easy to do at home with
     common household materials. 
-    tou WILL ONLY provide one project.
 
     You WILL ONLY provide one project. Be sure to focus on
     the project name, duration, difficulty, required materials 
@@ -26,8 +26,8 @@ science_craft_researcher = LlmAgent(
     name="ScienceCraftResearcher",
     model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
     instruction="""
-    Research popular and safe science-related crafts
-    or projects for toddlers that are easy to do at home with
+    Using google_search, research safe science-related crafts 
+    and projects for toddlers that are easy to do at home with
     common household materials. 
 
     You WILL ONLY provide one project. Be sure to focus on
@@ -49,7 +49,7 @@ parallel_craft_team = ParallelAgent(
 
 project_synthesizer = LlmAgent(
     name="ProjectSynthesizer",
-    model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
+    model=LiteLlm(model="ollama_chat/qwen3:30b"),
     instruction="""You are a project synthesizer. You will receive research for
     multiple toddler projects from the CraftResearchTeam. 
     
@@ -58,15 +58,27 @@ project_synthesizer = LlmAgent(
     provided research, or combine elements from them to create a new project.
 
     Research Outputs:
-
     - {art_project}
     - {science_project}
 
-    Your output MUST be a single `Project` object.
+    Format should look like the following:
+
+    **Name:** The name of the project
+    **Description:** A brief 2-3 sentence description of the project
+    **Duration:** The estimated duration of the project in minutes
+    **Materials:** A bulleted list of materials required for the project
+    **Instructions:** A numbered list of step-by-step instructions for the project
+
+    There should be no extra categories or preamble or comments after
+    the instructions.
     """,
-    output_schema=Project,
+    # output_schema=Project,
     output_key="standard_project",
 )
+
+# Your output MUST be a single `Project` object.
+# prolly should separate the project schema out from this.
+# one should be user facing, the other for future database manipulations
 
 root_agent = SequentialAgent(
     name="CraftResearchPipeline",
